6b724c364c35dab79a17b874bb34f251e98eeb1a
Socheat Sok <socheatsok78@gmail.com>
2021-12-26 15:11:54 +0700

    feat: remove `gnu-sed` dependency from Formula/sshpass.rb

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -4,8 +4,6 @@ class Sshpass < Formula
   url "https://sourceforge.net/projects/sshpass/files/sshpass/#{version}/sshpass-#{version}.tar.gz"
   sha256 "71746e5e057ffe9b00b44ac40453bf47091930cba96bbea8dc48717dedc49fb7"
 
-  depends_on "gnu-sed"
-
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
