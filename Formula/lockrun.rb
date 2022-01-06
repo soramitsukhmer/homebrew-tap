09d2f0d27162b8c6547c85e015d48aeb9d0d2665
Socheat Sok <alex@socheat.net>
2022-01-06 17:20:42 +0700

    feat: add conflicts_with to lockrun

--- Formula/lockrun.rb
+++ Formula/lockrun.rb
@@ -5,6 +5,8 @@ class Lockrun < Formula
   version "1.1.3"
   sha256 "b846c1c50f9994c3ebeba89e9c2cd1fdaba777e5e19f3e65bde0da4f0beed03d"
 
+  conflicts_with "lockrun"
+
   def install
     system ENV.cc, ENV.cflags, "lockrun.c", "-o", "lockrun"
     bin.install "lockrun"
