38fda9a0a35a67c708ee905a2a775cd2fd52dc9a
Socheat Sok <socheatsok78@gmail.com>
2022-01-13 18:53:44 +0700

    Update `certstrap` to v1.3.0

--- Formula/certstrap.rb
+++ Formula/certstrap.rb
@@ -1,8 +1,9 @@
 class Certstrap < Formula
   desc "Tools to bootstrap CAs, certificate requests, and signed certificates"
   homepage "https://github.com/soramitsukhmer/certstrap"
-  url "https://github.com/soramitsukhmer/certstrap/archive/refs/tags/v1.2.1.tar.gz"
-  sha256 "90fb1d55c70c760b7542ba4bd6b2341dc2128e729d2c151c4b11cd449595d087"
+  version "1.3.0"
+  url "https://github.com/soramitsukhmer/certstrap/archive/refs/tags/v#{version}.tar.gz"
+  sha256 "afc781c0ad0412bef29b8e48997b375be663b306cbd4e8c1c82f7464fe8e0ca6"
   license "Apache-2.0"
   
   conflicts_with "certstrap"
