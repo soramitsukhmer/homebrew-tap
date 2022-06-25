8a5a0736a4dea0fa32cbf51159acd324021c8d4f
Socheat Sok <socheatsok78@gmail.com>
2022-06-25 19:49:55 +0700

    Update certstrap.rb

--- Formula/certstrap.rb
+++ Formula/certstrap.rb
@@ -2,8 +2,8 @@ class Certstrap < Formula
   desc "Tools to bootstrap CAs, certificate requests, and signed certificates"
   homepage "https://github.com/soramitsukhmer/certstrap"
   version "1.3.2"
+  sha256 :no_check
   url "https://github.com/soramitsukhmer/certstrap/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "2e89a2606291423d98439f456f25cbec90f30f0156bfc9c2916e5098cc771a1a"
   license "Apache-2.0"
   
   conflicts_with "certstrap"
