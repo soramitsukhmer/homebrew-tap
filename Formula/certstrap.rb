1721cbdc85ae2f216bfc5dd84100edfea192f97c
Socheat <github@socheat.dev>
2026-05-18 17:21:45 +0700

    Update certstrap

--- Formula/certstrap.rb
+++ Formula/certstrap.rb
@@ -1,16 +1,18 @@
 class Certstrap < Formula
   desc "Tools to bootstrap CAs, certificate requests, and signed certificates"
-  homepage "https://github.com/socheatsok78-lab/certstrap"
+  homepage "https://github.com/socheatsok78/certstrap"
+  url "https://github.com/socheatsok78/certstrap/archive/refs/heads/master.tar.gz"
   version "latest"
-  url "https://github.com/socheatsok78-lab/certstrap/archive/refs/heads/master.zip"
+  sha256 "f3ca1d642e0e59ceea5f5cd44aa049fbeed67ebde1051f0dd35cd0f3573a6884"
   license "Apache-2.0"
-  
-  conflicts_with "certstrap"
+
   depends_on "go" => :build
+
   def install
     system "go", "build", "-ldflags", "-s -w -X main.release=#{version}", "-trimpath", "-o", bin/"certstrap"
     prefix.install_metafiles
   end
+
   test do
     system "#{bin}/certstrap", "init", "--common-name", "Homebrew Test CA", "--passphrase", "beerformyhorses"
   end
